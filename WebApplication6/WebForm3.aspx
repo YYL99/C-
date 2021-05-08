12b27ecc126f532421973c5187f9b5aa96fd6ee9
YYL99 <llaayy@protonmail.com>
2021-05-08 17:20:36 +0800

    Image

---
+++ WebApplication6/WebForm3.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm3.aspx.cs" Inherits="WebApplication6.WebForm3" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>图片</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:RadioButton ID="RadioButton1" runat="server" AutoPostBack="True" Text="男" GroupName="Group1" OnCheckedChanged="RadioButton1_CheckedChanged" />
+            <asp:RadioButton ID="RadioButton2" runat="server" AutoPostBack="True" Text="女" GroupName="Group1" OnCheckedChanged="RadioButton2_CheckedChanged" />
+        </div>
+        <div>
+
+            <asp:Image ID="Image1" runat="server" AlternateText="选择性别" />
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication6/WebForm3.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication6
+{
+    public partial class WebForm3 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void RadioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (RadioButton1.Checked == true)
+            {
+                Image1.ImageUrl = "./Image/男.jpg";
+            }
+            else
+            {
+                Image1.ImageUrl = "./Image/女.jpg";
+            }
+        }
+
+        protected void RadioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (RadioButton2.Checked == true)
+            {
+                Image1.ImageUrl = "./Image/女.jpg";
+            }
+            else
+            {
+                Image1.ImageUrl = "./Image/男.jpg";
+            }
+        }
+    }
+}
