656e212fb9d378026a2d2ab0d80d1642c3ea3dc4
YYL99 <llaayy@protonmail.com>
2021-05-08 16:05:28 +0800

    ListBox

---
+++ WebApplication6/WebForm1.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication6.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="True" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+                <asp:ListItem Value="0">男</asp:ListItem>
+                <asp:ListItem Value="1">女</asp:ListItem>
+            </asp:DropDownList>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication6/WebForm1.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication6
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (DropDownList1.SelectedValue)
+            {
+                case "0":
+                    Response.Write("你好，先生！");
+                    break;
+                case "1":
+                    Response.Write("你好，女士！");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
